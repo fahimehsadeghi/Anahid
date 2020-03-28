a609e8dd76aaa4580255e55ba36cfef2946cca75
fatemeh <55344988+zoha1366@users.noreply.github.com>
2020-03-28 10:23:06 +0430

    Delete routes.py

--- routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-import sys
-import os
-from Untitled12 import app
-@render_template.route("/")
-@render_template.route("/index")
-@render_template.route("/home")
-def index():
-    return "index.html", login
